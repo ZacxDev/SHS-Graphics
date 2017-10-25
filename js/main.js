a4ca05ef5d0cd0c825e8c52d3299e2a2002ee92e
ZacxDev <zacxdev@gmail.com>
2017-10-25 11:30:27 -0500

    Made things look veeeeeniiiceee

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
+  initParticleNet();
   initProjectListeners();
+  initFooterListeners();
+
+  //hidePopups();
 });
 
 $(window).load(function() {
@@ -22,4 +26,39 @@ function initProjectListeners()
     $(this).find('.desc_wrapper').css('display', 'none');
     $(this).find('#canvas').css('display', 'none');
   });
+}
+
+function initFooterListeners()
+{
+  $(document).on('mouseenter', '.hov_contact', function() {
+    $(this).parent().find('.contact_popup').show();
+  });
+
+  $(document).on('mouseenter', '.hov_libs', function() {
+    $(this).parent().find('.libs_popup').show();
+  });
+
+  $(document).on('mouseleave', '.hov_contact, .hov_libs', function() {
+    $(this).parent().find('.contact_popup, .libs_popup').hide();
+  });
+}
+
+function hidePopups()
+{
+  $(".contact_popup, .libs_popup").hide();
+}
+
+function initParticleNet()
+{
+  // Initialisation
+  var canvasDiv = document.getElementById('particle-canvas');
+  var options = {
+    //particleColor: '#11ce00',
+    particleColor: '#ff0000',
+    //background: 'https://raw.githubusercontent.com/JulianLaval/canvas-particle-network/master/img/demo-bg.jpg',
+    interactive: false,
+    speed: 'low',
+    density: 'high'
+  };
+  var particleCanvas = new ParticleNetwork(canvasDiv, options);
 }
